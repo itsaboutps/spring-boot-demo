31c084b38346f0f4c06c3ea3f22704c2d6009f6c
Prashant Singh <psprashantsingh91@gmail.com>
2024-05-03 01:16:57 +0530

    learning springboot

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # spring-boot-demo
+learnings:
+
+## 
+@Controller is used to mark classes as Spring MVC Controller.
+@RestController is a convenience annotation that does nothing more than adding the @Controller and @ResponseBody annotations (see: Javadoc)
+So the following two controller definitions should do the same
+
+@Controller
+@ResponseBody
+public class MyController { }
+
+@RestController
+public class MyRestController { }
+
+
+
+###
+Spring scans the classes in your application to identify beans, 
+which are typically annotated with stereotypes like @Component, 
+@Service, @Repository, or configured using @Bean.
+
+## to run spring boot application :: ./mvnw spring-boot:run
+
+## if you have to hit url you can use:: $ curl http://localhost:8080
+
+
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,54 @@
 package com.example.demo;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("I am started now");
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx){
+
+		return args->{
+			System.out.println("Lets inspect the beans provided by Spring boot");
+
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for(String beanName:beanNames){
+				System.out.println(beanName);
+				
+			}
+		};
+		
+	}
+
+	@Bean
+	public String getWelcomeString(){
+		return "Welcome to spring boot application";
+
+	}
+
+	// @PostConstruct
+	// public void printWelcomeString(){
+	// 	System.out.println(getWelcomeString());
+	// }
+
+	@PostConstruct
+	public void printWelcomeString(){
+		System.out.println("I am POST-CONSTRUCT AFTER SPRING INITIALISED ALL THE BEANS");
 	}
 
 }
